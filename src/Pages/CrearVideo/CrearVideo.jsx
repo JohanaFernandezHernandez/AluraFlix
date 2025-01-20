0d940a85d4125c7434e3b78b53ad8e704d27b413
Johana Fernandez <najofe5@gmail.com>
2025-01-20 20:12:12 +0100

    feat: Iniciacion de formulario nuevo video

--- src/Pages/CrearVideo/CrearVideo.jsx
+++ src/Pages/CrearVideo/CrearVideo.jsx
@@ -1,8 +1,21 @@
+import { TextField } from '@mui/material';
+
 export const CrearVideo = () => {
 
 return(
 <>
-<p>Pagina de crear Video</p>
+<h1>Nuevo Video</h1>
+<p>Complete el formulario para crear una nueva tarjeta de video</p>
+
+<form>
+
+</form>
+
+<TextField 
+        id="outlined-basic" 
+        label="Outlined" 
+        variant="outlined" 
+/>
 </>
 )
 }
